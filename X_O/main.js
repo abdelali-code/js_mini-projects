6365239a2c235fbba45d7db47deae50882f738eb
abdelali-code <57986145+abdelali-code@users.noreply.github.com>
2020-03-27 14:30:39 +0100

    add method to go one step back in the game

--- X_O/main.js
+++ X_O/main.js
@@ -1,48 +1,72 @@
-let btns = document.querySelectorAll(".row button");
-let player = document.getElementById("player");
-let winner = document.getElementById("winner");
+let res = [Array(9).fill(null)];
+const choices = document.querySelectorAll(".row button");
+const player = document.getElementById("player");
+const step_back_btn = document.getElementById("back");
+const winner = document.getElementById("winner");
+let is_win;
+let is_x_next = true;
 
-let isXnext = true;
-let res = Array(9).fill(null);
-
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", e => {
-    if (e.target.innerText != "X" && e.target.innerText != "0") {
-      if (isXnext == true) {
-        e.target.innerText = "X";
-        isXnext = false;
-        res[i] = "x";
-        player.textContent = "0";
-        // btns[i].style.backgroundColor = "green";
+for (let i = 0; i < choices.length; i++) {
+  choices[i].addEventListener("click", _ => {
+    // show and hide the return button
+    if (res.length >= 1) {
+      step_back_btn.style.display = "initial";
+    }
+    // show the which player will player
+    player.textContent = !is_x_next ? "X" : "O";
+    // copy the content of the last array in the res array;
+    const arr = [...res[res.length - 1]];
+    // modify the plce where user is clicked;
+    if (arr[i] != "x" && arr[i] != "o") {
+      if (is_x_next) {
+        arr[i] = "x";
+        is_x_next = !is_x_next;
       } else {
-        e.target.innerText = "0";
-        isXnext = true;
-        res[i] = "o";
-        player.textContent = "X";
+        arr[i] = "o";
+        is_x_next = !is_x_next;
       }
-      if (checkWinner(res)) {
-        winner.textContent = "the winner is :" + checkWinner(res);
-      }
-    } else {
-      alert("this case already selected");
+    }
+    res.push(arr);
+    updateUi();
+    is_win = checkWinner(res[res.length - 1]);
+    if (is_win) {
+      winner.textContent = `the winner is ${is_win}`;
     }
   });
 }
-// reset the game
-function rejower() {
-  const resetBtn = document.getElementById("reset");
-  resetBtn.style.display = "none";
-  res.fill(null);
-  for (let btn of btns) {
-    isXnext = true;
-    btn.style.backgroundColor = "white";
-    btn.textContent = "";
-    winner.textContent = "";
+// update the html from the data of the res array
+function updateUi() {
+  for (let i = 0; i < 9; i++) {
+    choices[i].textContent = res[res.length - 1][i];
   }
 }
 
+// back one step in the game
+function back() {
+  if (res.length > 1) {
+    player.textContent = !is_x_next ? "X" : "O";
+    is_x_next = !is_x_next;
+
+    res.pop();
+    updateUi();
+  }
+}
+
+//  to start the game again
+function rejower() {
+  // free the res to return to initial state
+  res = [Array(9).fill(null)];
+  for (let i = 0; i < 9; i++) {
+    choices[i].textContent = res[res.length - 1][i];
+    choices[i].style.backgroundColor = "#fff";
+  }
+  //
+  winner.textContent = "";
+  // hide the back button
+  step_back_btn.style.display = "none";
+}
+// check if exist a winner
 function checkWinner(sq) {
-  const btn = document.getElementById("reset");
   let prob = [
     [0, 1, 2],
     [3, 4, 5],
@@ -58,10 +82,9 @@ function checkWinner(sq) {
     let [a, b, c] = prob[i];
     if (sq[a] != null && sq[a] === sq[b] && sq[a] === sq[c]) {
       let color = sq[a] === "x" ? "green" : "red";
-      btns[a].style.backgroundColor = color;
-      btns[b].style.backgroundColor = color;
-      btns[c].style.backgroundColor = color;
-      btn.style.display = "initial";
+      choices[a].style.backgroundColor = color;
+      choices[b].style.backgroundColor = color;
+      choices[c].style.backgroundColor = color;
       return sq[a];
     }
   }
